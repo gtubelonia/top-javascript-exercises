c8db21ce5a9fe7dc2d66bfa8ccaf4e067926331a
Gian <gtubelonia@live.com>
2024-04-10 17:26:35 -0500

    finished exercise 4. learned a bit about using array.filter

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(ary, ...valuesToRemove) {
+    const newArray = ary.filter(item => !valuesToRemove.includes(item));
+    return newArray;
 };
+
+function checkRemovableValue(values){
+
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
