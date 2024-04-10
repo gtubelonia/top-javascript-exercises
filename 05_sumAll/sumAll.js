8d411207d8692ac17e6fa5c10c1c89c974d56472
Gian <gtubelonia@live.com>
2024-04-10 17:36:42 -0500

    finished 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+    let sum = 0;
 
+    if (start > end){
+        let temp = start;
+        start = end;
+        end = temp;
+    }
+
+    if (!Number.isInteger(start) || !Number.isInteger(end)){
+        return 'ERROR';
+    }
+    if ((start < 0) || (end < 0)) {
+        return "ERROR";
+    }
+
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
