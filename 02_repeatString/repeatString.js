8a2c8038b63222063d08758a09933469ccd9f7cc
Gian <gtubelonia@live.com>
2024-04-10 16:44:10 -0500

    finished exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(word, value) {
+    let stringRepeat = "";
 
+    if(value < 0){
+        return "ERROR";
+    }
+    for(let i = 0; i < value; i++){
+        stringRepeat += word;
+    }
+
+    return stringRepeat;
 };
 
 // Do not edit below this line
