0a414a9e49970da272f541a7f1da7713a5733c05
Gian <gtubelonia@live.com>
2024-04-10 17:00:19 -0500

    finished exercise 3. keep in mind reverse and split string functions

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let reverse = "";
+    for(let i = 0; i < word.length; i++){
+        reverse += word[word.length-i-1];
+    }
+    return reverse;
 };
 
 // Do not edit below this line
