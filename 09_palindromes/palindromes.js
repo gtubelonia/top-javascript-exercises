a907f6fa018fd7cf3d7719d854456fd1dcf0e055
Gian <gtubelonia@live.com>
2024-04-16 14:27:06 -0500

    finished 9. the solution looked much more elegant

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
+const palindromes = function (palindrome) {
+    let cleanPal = palindrome.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").split(" ").join("").toLowerCase();
+    let clean = cleanPal.split("");
+    let rvrs = cleanPal.split("").reverse();
 
+    for(let i = 0; i < clean.length ; i++){
+        if (clean[i]!= rvrs[i]){
+            return false;
+        }
+    }
+
+    return true;
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+palindromes("A car, a man, a maraca.");
