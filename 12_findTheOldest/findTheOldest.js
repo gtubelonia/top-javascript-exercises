10d248b2622deae3a8da6804bece68b8aae94457
Gian <gtubelonia@live.com>
2024-04-16 15:18:56 -0500

    finished exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,44 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldest = people.reduce( (oldestPerson, currentPerson) =>{
+        if(!oldestPerson.yearOfBirth){
+            return currentPerson;
+        }
+        if(!oldestPerson.yearOfDeath){
+            oldestPerson.yearOfDeath = new Date().getFullYear();
+        }
+        if(!currentPerson.yearOfDeath){
+            currentPerson.yearOfDeath = new Date().getFullYear();
+        }
+        let oldestAge = oldestPerson.yearOfDeath - oldestPerson.yearOfBirth;
+        let currentAge = currentPerson.yearOfDeath - currentPerson.yearOfBirth;
 
+        if (oldestAge < currentAge){
+            oldestPerson = currentPerson;
+        }
+
+        return oldestPerson;
+    } , {name: "", yearOfBirth:0, yearOfDeath:0});
+
+    return oldest;
 };
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
+findTheOldest( people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1942,
+      yearOfDeath: 1970,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]);
