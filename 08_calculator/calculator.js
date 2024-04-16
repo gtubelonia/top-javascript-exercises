7a122c4df002af8cae1c2e23df557ce986344650
Gian <gtubelonia@live.com>
2024-04-16 13:47:21 -0500

    finished 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (num) {
+  let sum = num.reduce((number, val) =>
+    number + val, 0);
+  return parseInt(sum);
 };
 
-const multiply = function() {
-
+const multiply = function (numArray) {
+  let product = numArray.reduce((prod, currentValue) => prod * currentValue, 1);
+  return parseInt(product);
 };
 
-const power = function() {
-	
+const power = function (base, power) {
+  return parseInt(base ** power);
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let prod = 1;
+  
+  if (num < 0) {
+    return -1;
+  } else if (num == 0) {
+    return 1;
+  } else {
+    for (let i = num; i > 1; i--) {
+      prod *= i;
+    }
+  }
+  return prod;
 };
 
 // Do not edit below this line
