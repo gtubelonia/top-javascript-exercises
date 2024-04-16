6194ab721b169523d477f3f5214f4c0e315def48
Gian <gtubelonia@live.com>
2024-04-16 14:43:15 -0500

    finished 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let previousNum1 = 1, sum = 0, previousNum2 = 1;
 
+    if (num < 0){
+        return "OOPS";
+    }
+    if(num == 0){
+        return 0;
+    } 
+    if(num == 1 || num == 2){
+        return 1;
+    }
+
+    for(let i = 2; i < num; i++){
+        sum = previousNum1 + previousNum2;
+        previousNum2 = previousNum1;
+        previousNum1 = sum;
+    }
+    return sum;
 };
 
 // Do not edit below this line
